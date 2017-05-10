1061d981820e210d841c42d961f6ba5b1cae1813
carloluis <carloluisr@gmail.com>
2017-05-10 13:01:35 -0300

    + prop-types check

--- app/components/MyButton.jsx
+++ app/components/MyButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MyButton = ({ children }) => (
 	<div>
@@ -7,5 +8,9 @@ const MyButton = ({ children }) => (
 		</button>
 	</div>
 );
+
+MyButton.propTypes = {
+	children: PropTypes.string
+};
 
 export default MyButton;
