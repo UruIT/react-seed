0c07b57b0392c3726acb8884530d30628ea4e791
carloluis <carloluisr@gmail.com>
2017-05-10 12:52:38 -0300

    fetch utils (with polyfills) * getJson

---
+++ app/utils/fetch.js
@@ -0,0 +1,20 @@
+import 'es6-promise/auto';
+import 'whatwg-fetch';
+
+const _errorHandle = response => {
+	if (response.ok) {
+		return response;
+	}
+
+	let error = new Error(response.statusText);
+	error.status = response.statusText;
+	error.response = response;
+
+	throw error;
+};
+
+const _parseJson = response => response.json();
+
+export const getJson = url => fetch(url).then(_errorHandle).then(_parseJson);
+
+export default { getJson };
