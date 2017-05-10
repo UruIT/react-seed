91ea8cb6de61d9de5f148b809c39fcf342dcc8f6
carloluis <carloluisr@gmail.com>
2017-05-10 00:05:53 -0300

    import fix for case-sensitive systems

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app.jsx';
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
