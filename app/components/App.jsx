6fa01d3617bb866cfda3dfcc2f43dee71a3e1fa3
Carloluis Rodríguez <carloluisr@gmail.com>
2017-05-11 15:21:47 -0300

    features/fetch (#3)
    * Webpack configuration by environment
    
    * Fix indentation issues
    
    * Fix: clean plugin isn't working
    
    * Setup CSS modules
    
    * Fix indentation
    
    * Update readme
    
    * import fix for case-sensitive systems
    
    * PR feedback
    
    * fetching some data..
    * from `api.chucknorris.io`
    
    * fetch utils (with polyfills)
    * getJson
    
    * fetch fix (IE) - using polyfills
    
    * + prop-types check
    
    * <App> component update
    * fetch on click the `Test` btn
    
    * webpack (test) - typo fix
    
    * remove unused <MyButton> component
    
    * <MyButton> + warn to console
    * enable linting for dev

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import MyButton from './MyButton/MyButton';
+import { getJson } from '../utils/fetch';
 import styles from './App.scss';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			joke: ''
+		};
 	}
-
+	handleClick = () => {
+		getJson('https://api.chucknorris.io/jokes/random')
+			.then(res => this.setState({
+				joke: res.value
+			}));
+	}
 	render() {
 		return (
-			<div>
-				<button className={styles.button} onClick={this.test}>Test</button>
-				<MyButton></MyButton>
+			<div className={styles.container}>
+				<button className={styles.button} onClick={this.handleClick}>Test</button>
+				<MyButton>{ this.state.joke }</MyButton>
 			</div>
 		);
 	}
