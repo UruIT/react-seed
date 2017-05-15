35fe360f417048382e4077153a4339d4b9bcd069
Rodrigo Rivera <rrivem@gmail.com>
2017-05-15 10:31:20 -0300

    Jest (#5)
    * Webpack configuration by environment
    
    * Fix indentation issues
    
    * Fix: clean plugin isn't working
    
    * Setup CSS modules
    
    * Fix indentation
    
    * Update readme
    
    * import fix for case-sensitive systems
    
    * PR feedback
    
    * fetching some data..
    * from `api.chucknorris.io`
    
    * fetch utils (with polyfills)
    * getJson
    
    * fetch fix (IE) - using polyfills
    
    * + prop-types check
    
    * <App> component update
    * fetch on click the `Test` btn
    
    * webpack (test) - typo fix
    
    * adding react-router-dom
    
    * about page to test routing
    
    * Links for routed components
    
    * Fallback path
    
    * adding jest with simple tests
    
    * jest --watchAll to run all test while tdd
    
    * snapshot testing with react-test-renderer
    
    * simple support for import of styles modules
    
    * adding enzyme to test UI events
    
    * test promise and state update
    
    * fix for unit tests
    
    * removing console statement
    
    * keep test files in the same folder the module under test

---
+++ app/components/about/About.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import About from './About';
+import renderer from 'react-test-renderer';
+
+test('renders About', () => {
+	const tree = renderer.create(
+		<About />
+	).toJSON();
+
+	expect(tree).toMatchSnapshot();
+});
