8764f3f57c40a0d8b2696fd49558ba2a484a24eb
carloluis <carloluisr@gmail.com>
2017-05-17 10:40:52 -0300

    About component test (refactor)

--- app/components/about/__tests__/About.spec.js
+++ app/components/about/__tests__/About.spec.js
@@ -3,9 +3,7 @@ import About from '../About';
 import renderer from 'react-test-renderer';
 
 test('renders About', () => {
-	const tree = renderer.create(
-		<About />
-	).toJSON();
+	const tree = renderer.create(<About />).toJSON();
 
 	expect(tree).toMatchSnapshot();
 });
