67fb25dc37b4fdb0fe298edec842b51f82e3f6e9
carloluis <carloluisr@gmail.com>
2017-05-18 11:31:28 -0300

    remove some snapshots * composite components snapshots must be created from shallow rendering, so we can test separately those inner components

--- app/components/nav/Nav.spec.js
+++ app/components/nav/Nav.spec.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import Nav from './Nav';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import renderer from 'react-test-renderer';
-import withContext from '../__mocks__/context-hoc';
 
 test('<Nav/>', () => {
-	const ROUTER = {
-		router: {
-			route: {
-				location: jest.fn()
-			},
-			history: {
-				createHref: jest.fn(),
-				replace: jest.fn(),
-				push: jest.fn()
-			}
-		}
-	};
-	const NavWithContext = withContext(ROUTER)(Nav);
-	const tree = renderer.create(<NavWithContext />).toJSON();
-
-	expect(tree).toMatchSnapshot();
-});
-
-test('shallow <Nav/>', () => {
 	const renderer = new ShallowRenderer();
 	const tree = renderer.render(<Nav />);
 
--- app/components/notFound/NotFound.spec.js
+++ app/components/notFound/NotFound.spec.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import NotFound from './NotFound';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import renderer from 'react-test-renderer';
-import withContext from '../__mocks__/context-hoc';
 
 test('<NotFound/>', () => {
-	const ROUTER = {
-		router: {
-			history: {
-				createHref: jest.fn(),
-				push: jest.fn(),
-				replace: jest.fn()
-			}
-		}
-	};
-	const NotFoundWithContext = withContext(ROUTER)(NotFound);
-
-	const tree = renderer.create(<NotFoundWithContext />).toJSON();
-
-	expect(tree).toMatchSnapshot();
-});
-
-test('shallow <NotFound/>', () => {
 	const renderer = new ShallowRenderer();
 
 	const tree = renderer.render(<NotFound />);
