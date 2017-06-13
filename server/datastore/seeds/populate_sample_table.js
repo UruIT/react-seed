2fd9b198a115b6f8669b361ca19a3a199727b5e5
Carloluis Rodríguez <carloluisr@gmail.com>
2017-06-13 18:06:24 -0300

    fix/pre-commit (#23)
    * setup server test with tape and supertest
    
    * add server sample tests
    
    * add server db seeds..
    
    * [server] package.json scripts update
    
    * linting fixes..
    
    * eslintrc config update - precommit hook
    * add specific server config for eslint
    
    * eslint error fix
    
    * .eslintrc refactor (react specific) -> ./client/.eslintrc

--- server/datastore/seeds/populate_sample_table.js
+++ server/datastore/seeds/populate_sample_table.js
@@ -1,5 +1,5 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function(knex) {
 	// Deletes ALL existing entries
 	return knex('sample').del().then(function() {
 		// Inserts seed entries
