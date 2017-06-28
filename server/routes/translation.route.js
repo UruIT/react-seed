beccd9c6bd665ea19c90d06437cbdd6160f102c0
Marina Acosta <marin2592@gmail.com>
2017-06-28 14:32:47 -0300

    Server test for POC i18n

--- server/routes/translation.route.js
+++ server/routes/translation.route.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const zh = require('../dictionaries/zh');
-const ja = require('../dictionaries/ja');
-const ru = require('../dictionaries/ru');
-const en = require('../dictionaries/en');
-
-
-function getDictionary(locale) {
-	switch (locale) {
-		case 'zh':
-			return zh;
-		case 'ja':
-			return ja;
-		case 'ru':
-			return ru;
-		default:
-			return en;
-	}
-}
+const translation = require('../services/translation.service');
 
 router.get('/:locale', (request, response) => {
-	let translation = getDictionary(request.params.locale).messages;
-	response.send(translation);
+	let messages = translation.getDictionary(request.params.locale).messages;
+	response.send(messages);
 });
 
 module.exports = router;
