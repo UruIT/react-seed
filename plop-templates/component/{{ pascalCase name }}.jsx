c31d763774c61ae2211e35cf31279a6cf13dd015
Matias Delgado <matiasdel@gmail.com>
2017-08-22 10:41:31 -0300

    Add plop with component template

---
+++ plop-templates/component/{{ pascalCase name }}.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './{{ dashCase name }}.scss';
+
+export default class {{ pascalCase name }} extends React.Component {
+	render() {
+		<div />
+	}
+}
+
+{{ pascalCase name }}.propTypes = {
+
+};
---
+++ plop-templates/component/{{ pascalCase name }}.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {{ pascalCase name }} from './{{ pascalCase name }}';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+const PROPS = {
+
+}
+
+describe('{{ pascalCase name }}', () => {
+	it('should render', () => {
+		const wrapper = shallow(<{{ pascalCase name }} {...PROPS} />);
+		expect(wrapper.find('div').node).toBeTruthy();
+	});
+});
