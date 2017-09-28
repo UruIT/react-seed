625854d0801f6cb870001bc0855870e79b085f0f
carloluis <carloluisr@gmail.com>
2017-09-28 10:06:40 -0300

    <counter/> specs (100% coverage)

--- client/components/counter/Counter.spec.js
+++ client/components/counter/Counter.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Counter from './Counter';
 
 describe('<Counter/>', () => {
@@ -8,5 +9,30 @@ describe('<Counter/>', () => {
 		const tree = renderer.render(<Counter />);
 
 		expect(tree).toMatchSnapshot();
+	});
+});
+
+describe('Counter', () => {
+	let tree;
+	beforeEach(() => {
+		tree = shallow(<Counter />);
+	});
+
+	it('should start with value in zero', () => {
+		expect(tree.state()).toEqual({ value: 0 });
+	});
+
+	it('should decrement value', () => {
+		const minus = tree.find('a').first();
+		minus.simulate('click');
+
+		expect(tree.state()).toEqual({ value: -1 });
+	});
+
+	it('should increment value', () => {
+		const plus = tree.find('a').last();
+		plus.simulate('click');
+
+		expect(tree.state()).toEqual({ value: 1 });
 	});
 });
