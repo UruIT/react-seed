080ce76916d5d8a7f542e2ca4a4b36a4ee09eeeb
Mathías Rabellino <mathiasrabellino@gmail.com>
2019-03-16 18:15:10 -0300

    react lazy and suspense

--- client/routes/Root.js
+++ client/routes/Root.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import App from '../components/app';
-import About from '../components/about';
 import NotFound from '../components/not-found';
 import Nav from '../components/nav/Nav';
 
 import links from './links';
 
+const App = lazy(() => import('../components/app'));
+const About = lazy(() => import('../components/about'));
+
 const Root = () => (
 	<BrowserRouter>
 		<div>
 			<Nav />
-			<Switch>
-				<Route exact path={links.index} component={App} />
-				<Route path={links.about} component={About} />
-				<Route component={NotFound} />
-			</Switch>
+			<Suspense fallback={'Loading...'}>
+				<Switch>
+					<Route exact path={links.index} component={App} />
+					<Route path={links.about} component={About} />
+					<Route component={NotFound} />
+				</Switch>
+			</Suspense>
 		</div>
 	</BrowserRouter>
 );
